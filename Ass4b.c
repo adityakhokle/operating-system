2feee4d2feae9e83d23989283ceca28e11cadefa
adityakhokle <149953141+adityakhokle@users.noreply.github.com>
2023-11-06 17:18:17 +0530

    Update Ass4b.c

--- Ass4b.c
+++ Ass4b.c
@@ -5,72 +5,82 @@ Application to demonstrate: Reader-
 Writer problem with reader priority.
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<unistd.h>
 
-#define NUM_READERS 3
-#define NUM_ITERATIONS 5
+void *writer_thr(int temp);
+void *reader_thr(int temp);
+pthread_mutex_t mutex;
+sem_t wrt;
+int readcount=0,nwt,nrd;
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_barrier_t barrier;
-int shared_data;
 
-void *writer(void *arg)
+void main()
 {
-    for (int i = 0; i < NUM_ITERATIONS; i++)
-    {
-        int random_number = rand() % 100; // Generate a random number
+  long int i;
+  pthread_mutex_init(&mutex,0);
+  sem_init(&wrt,0,1);
+  pthread_t reader[100],writer[100];
+  printf("\n Enter number of readers:");
+  scanf("%d",&nrd);
+  printf("\n Enter number of writers:");
+  scanf("%d",&nwt);
+ 
+  for(i=1;i<=nwt;i++)
+  {
+    pthread_create(&writer[i],NULL,(void *)writer_thr,(int *)i);
+    pthread_join(writer[i],NULL);
+  }
+ 
+  for(i=1;i<=nrd;i++)
+  {
+    pthread_create(&reader[i],NULL,(void *)reader_thr,(int *)i);
+       pthread_join(reader[i],NULL);
+  }
+ 
+    
+  sem_destroy(&wrt);
+  pthread_mutex_destroy(&mutex);
 
-        pthread_mutex_lock(&mutex);
-        shared_data = random_number;
-        printf("Writer wrote: %d\n", shared_data);
-        pthread_mutex_unlock(&mutex);
-
-        sleep(1); // Simulate some writing time
-    }
-    pthread_exit(NULL);
 }
 
-void *reader(void *arg)
+
+void *reader_thr(int temp)
 {
-    int reader_id = *((int *)arg);
-    for (int i = 0; i < NUM_ITERATIONS; i++)
-    {
-        pthread_barrier_wait(&barrier);
-
-        pthread_mutex_lock(&mutex);
-        int data = shared_data;
-        printf("Reader %d: Item READ from buffer: %d\n", reader_id, data);
-        pthread_mutex_unlock(&mutex);
-
-        sleep(1); // Simulate some reading time
-    }
-    pthread_exit(NULL);
+ 
+  printf("\n Reader %d is trying to enter database for reading.",temp);
+   pthread_mutex_lock(&mutex);
+   readcount++;
+   if(readcount==1)
+   sem_wait(&wrt);
+    pthread_mutex_unlock(&mutex);
+ 
+  printf("\nReader %d is now reading in database.",temp);
+ 
+ 
+   pthread_mutex_lock(&mutex);
+  readcount--;
+  if(readcount==0)
+ 
+  sem_post(&wrt);  
+   pthread_mutex_unlock(&mutex);
+  printf("\nReader %d has left the database.\n",temp);  
+  sleep(3);
 }
 
-int main()
+void *writer_thr(int temp)
 {
-    srand(time(NULL));
-    pthread_t writer_thread, reader_threads[NUM_READERS];
-    int reader_ids[NUM_READERS] = {1, 2, 3};
 
-    pthread_barrier_init(&barrier, NULL, NUM_READERS);
-
-    pthread_create(&writer_thread, NULL, writer, NULL);
-    for (int i = 0; i < NUM_READERS; i++)
-    {
-        pthread_create(&reader_threads[i], NULL, reader, &reader_ids[i]);
-    }
-
-    pthread_join(writer_thread, NULL);
-    for (int i = 0; i < NUM_READERS; i++)
-    {
-        pthread_join(reader_threads[i], NULL);
-    }
-
-    pthread_barrier_destroy(&barrier);
-
-    return 0;
+ printf("\nWriter %d is trying to enter database for modifying data",temp);
+ sem_wait(&wrt);
+ printf("\n Writer %d is writing in database.",temp);
+ sleep(3);
+ printf("\n Writer %d is leaving the database.\n",temp);
+ sem_post(&wrt);
 }
+
+
+
