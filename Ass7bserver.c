434231a7b055b60c6ab38904ba06d5d76479fbca
adityakhokle <149953141+adityakhokle@users.noreply.github.com>
2023-11-06 23:56:36 +0530

    Update Ass7bserver.c

--- Ass7bserver.c
+++ Ass7bserver.c
@@ -16,33 +16,32 @@ segment and displays it to the screen.
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MAXSIZE 27
+#define MAXSIZE 27 // Define a constant for the maximum size of the shared memory segment
 
-void die(char *s)
-{
-    perror(s);
+void die(char *s) {
+    perror(s); // Function to display an error message and exit the program
     exit(1);
 }
 
-int main()
-{
-    int shmid, retval;
+int main() {
+    int shmid, retval; // Declare 'retval' here to store return values
     key_t key;
-    char *shm, *s;
+    char *shm, *s; // Declare pointers to shared memory and a character pointer
 
-    key = 5678;
+    key = 5678; // Define a key for the shared memory segment
 
+    // Create or access the shared memory segment with key '5678' and size 'MAXSIZE' (IPC_CREAT specifies to create if it doesn't exist, 0666 specifies permissions)
     if ((shmid = shmget(key, MAXSIZE, IPC_CREAT | 0666)) < 0)
         die("shmget");
 
+    // Attach to the shared memory segment and get a pointer to it
     if ((shm = shmat(shmid, NULL, 0)) == (char *)-1)
         die("shmat");
 
     printf("Enter a string (up to 26 characters): ");
-    if (fgets(shm, MAXSIZE, stdin) == NULL)
-    {
+    if (fgets(shm, MAXSIZE, stdin) == NULL) {
         printf("Input error\n");
-        exit(1);
+        exit(1); // Handle input error
     }
 
     /*
@@ -51,14 +50,14 @@ int main()
      * to '*', indicating that it has read what
      * we put there.
      */
-    while (*shm != '*')
-        sleep(1);
-    retval = shmdt(shm);
-    if (retval < 0)
-    {
+    while (*shm != '*') {
+        sleep(1); // Sleep for 1 second and check if the first character of shared memory is '*'
+    }
+    retval = shmdt(shm); // Detach from the shared memory segment
+    if (retval < 0) {
         printf("detachment failure");
-        return 0;
+        return 0; // Handle detachment failure
     }
 
-    exit(0);
+    exit(0); // Exit the program
 }
