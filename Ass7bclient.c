b889de1bb41bdbca76add95121ca0523203b3340
adityakhokle <149953141+adityakhokle@users.noreply.github.com>
2023-11-06 23:55:03 +0530

    Update Ass7bclient.c

--- Ass7bclient.c
+++ Ass7bclient.c
@@ -15,46 +15,47 @@ segment and displays it to the screen.
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXSIZE 27
+#define MAXSIZE 27 // Define a constant for the maximum size of the shared memory segment
 
-void die(char *s)
-{
-    perror(s);
+void die(char *s) {
+    perror(s); // Function to display an error message and exit the program
     exit(1);
 }
 
-int main()
-{
-    int shmid, retval; // Declare 'retval' here
+int main() {
+    int shmid, retval; // Declare 'retval' here to store return values
     key_t key;
-    char *shm, *s;
+    char *shm, *s; // Declare pointers to shared memory and a character pointer
 
-    key = 5678;
+    key = 5678; // Define a key for the shared memory segment
 
+    // Create or access the shared memory segment with key '5678' and size 'MAXSIZE' (0666 specifies permissions)
     if ((shmid = shmget(key, MAXSIZE, 0666)) < 0)
         die("shmget");
 
+    // Attach to the shared memory segment and get a pointer to it
     if ((shm = shmat(shmid, NULL, 0)) == (char *)-1)
         die("shmat");
 
     // Now read what the server put in the memory.
-    for (s = shm; *s != '\0'; s++)
-        putchar(*s);
-    putchar('\n');
+    for (s = shm; *s != '\0'; s++) {
+        putchar(*s); // Print the characters stored in the shared memory segment
+    }
+    putchar('\n'); // Print a newline character
 
     /*
      * Change the first character of the
-     * segment to '*', indicating we have read
-     * the segment.
+     * segment to '*', indicating that the segment has been read.
      */
-    *shm = '*';
-    retval = shmdt(shm);
-    if (retval < 0)
-    {
+    
+    *shm = '*'; // Change the first character of the shared memory segment to '*'
+    retval = shmdt(shm); // Detach from the shared memory segment
+    if (retval < 0) {
         printf("detachment failure");
-        return 0;
+        return 0; // Handle detachment failure
     }
-    retval = shmctl(shmid, IPC_RMID, NULL);
+    retval = shmctl(shmid, IPC_RMID, NULL); // Remove the shared memory segment
+    // The 'shmctl' function with 'IPC_RMID' removes the shared memory segment entirely
 
-    exit(0);
+    exit(0); // Exit the program
 }
